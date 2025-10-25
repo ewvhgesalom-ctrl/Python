049488b40555f5013fdc29ce4a07c3279c224021
NAme <ewvhgesalom@gmail.com>
2025-10-25 17:57:56 +0500

    sonlar.py fayli yaratildi
    Bu faylda sonlar bilan ishlash uchun masala yechimlari bor

---
+++ sonlar.py
@@ -0,0 +1,255 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+# =============================================================================
+# #print("Hello world")
+# #ism = input("Ismingizni kiriting : ")
+# yosh = input("yosh kirit :")
+# yosh = int(yosh)
+# print(yosh+1)
+# =============================================================================
+
+# =============================================================================
+# # abc son kiritilsa bca shaklida qaytaruvchi son tuzing
+# son  =  int(input("3 xonali son kiriting >> "))
+# natija = (son % 100) * 10 + son // 100
+# print(natija)
+# 
+# =============================================================================
+# =============================================================================
+# a = int(input("son kirit :"))
+# b = float(input("O'nlik son kirit : "))
+# =============================================================================
+
+
+# + qo'shish
+
+# - ayirish
+# * ko'paytirish
+# / bo'lish
+# // bo'lib butun qismini olish 10 // 3 = 3, 21 // 2 = 10
+# % bo'lib qoldiq qismini olib beradi 10 % 3 = 1, 25 % 7 = 4
+# ** daraja 2 ** 3 = 8, 3**2=9, 16 ** 0.5 = 4
+
+
+# =============================================================================
+# # masala_1 
+# L = int(input("L = "))
+# L_metr = L // 100
+# print(L_metr)
+# =============================================================================
+
+# =============================================================================
+# # masala_8  ab ba ab %10 = b b*10 ab//10 = a
+# son = int(input("Ikki xonali son kirit : "))
+# natija = (son % 10) * 10 + son // 10
+# print(natija)
+# =============================================================================
+
+# =============================================================================
+# yosh = 25
+# ism = 'Laylo'
+# familiya = "Abdrimova"
+# print(familiya + ' ' + ism)
+# =============================================================================
+
+#ism = input("Ismingizni kiriting : ")
+#print("salom", "kenjayev diyorbek  hjfgwhjefgehwjf jhdgDjhq".capitalize())
+
+# upper() matnni hamma harflarini bosh harfga o'zgartiradi
+# lower() harflarni kichik qiladi
+# title() so'zlarni 1-harfini katta qiladi
+# capitalize() Faqat birinchi so'zni birinchi harfini katta
+
+
+# Diyorbek DIYorbek dIYorbek
+
+    
+# =============================================================================
+# ismlar = ["Abdullajon", "Javohir", "Muslima", "Lazizbek", "Ergashboy","Diyorbek"]
+# # lists = [qiymat1, qiymat2, qiymat3, ... , qiymatn]
+# print(ismlar[0].upper())
+# 
+# =============================================================================
+# =============================================================================
+# # masala 14
+# abc = int(input("3 xonali son kiriting : "))
+# cab = (abc % 10)*100 + abc // 10
+# print(cab)
+# =============================================================================
+
+# =============================================================================
+# #masala 19
+# N_sekund  = int(input())
+# N_minut = N_sekund // 60
+# print(N_minut)              
+# 
+# =============================================================================
+
+sonlar  = [2,5,5,445,54,56,98,3]
+
+# =============================================================================
+# print(sonlar)
+sonlar.sort(reverse=True)
+# print(sonlar)
+# 
+# =============================================================================
+
+
+# =============================================================================
+# mevalar = ["olma", "anor", "anjir", "Shaftoli"]
+# 
+# print(mevalar)
+# print(sorted(mevalar))
+# 
+# print('uzum' in mevalar)
+# 
+# print(mevalar.index("behi"))
+# =============================================================================
+
+# =============================================================================
+# 
+# print(list(range(2,21,4)))
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# baholar = []
+# hafta_kunlari = ["Dushanba", "Seshanba", "Chorshanba"]
+# baholar = [4,5,5,3,4]
+# sonlar = [554,45,25,31,557,996,12]
+# 
+# print(f"Sonlar orasida eng kattasi {max(sonlar)}")
+# ism = "Ali"
+# sharif = "Valiyev"
+# ism_sharif = f"{ism} {sharif}"
+# 
+# print(f"Sonlar yig'indisi {sum(sonlar)}")
+# 
+# sonlar.append(69)
+# 
+# sonlar2 = sonlar
+# sonlar2.append(100)
+# sonlar.append(123)
+# 
+# sonlar3 = sonlar[:]
+# sonlar3.append(4569)
+# 
+# =============================================================================
+# =============================================================================
+# a = float(input("a = "))
+# P = 4 * a
+# print(P)
+# 
+# =============================================================================
+# *,/,+,-   ** -daraja, //-bo'lib butun qismni oladi
+# % - bo'lib qoldiq qismni oladi
+
+# =============================================================================
+# N =  int(input("N = "))
+# N_minut = N // 60
+# N_sekund = N % 60
+# print(f"Kun boshidan beri {N_minut} minut va {N_sekund} sekund o'tdi")
+# 
+# familiya = "ollanazarova"
+# print(familiya)
+# =============================================================================
+
+# =============================================================================
+# familiya = "fdghwefd"
+# print(f"salom, {familiya}")
+# =============================================================================
+
+# =============================================================================
+# ism1 = "Zedux"
+# ism2 = "Diyorbek"
+# ismFamiliya = "dhdfh dshjgd"
+# group= "Group 10"
+# print(group)
+# group = "Group 11"
+# print(group)
+# group = "python"
+# 
+# a = 20
+# b = 30
+# 
+# a = b
+# b = a
+# =============================================================================
+
+
+# =============================================================================
+# A = int(input("A = "))
+# print(f"A soni musbat : {A>0}")
+# =============================================================================
+
+# =============================================================================
+# A = int(input("A = "))
+# ifoda = (A % 2 == 1)
+# print(f"{A} soni toq son : {ifoda}")
+# =============================================================================
+
+
+# =============================================================================
+# A = int(input("A = "))
+# B = int(input("B = "))
+# 
+# =============================================================================
+# A va B sonlari toq
+# =============================================================================
+# ifoda = (A % 2 == 1 and B % 2 == 1)
+# print(ifoda)
+# =============================================================================
+# and -  ikkalasi bir vaqtda 
+# or  - ikkitasidan biri 
+
+#A , B, C
+# A>0, B>0, C<0
+
+
+ABC = int(input("son = "))
+c = ABC % 10
+A = ABC // 100
+B = ABC // 10 % 10
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
